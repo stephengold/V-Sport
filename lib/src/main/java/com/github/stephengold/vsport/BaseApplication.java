be7b393b599b49135147f8bb74d327bbfa941f28
Stephen Gold <sgold@sonic.net>
2023-07-15 19:38:47 -0400

    BaseApplication:  add capability to optimize depth-buffer layout

--- lib/src/main/java/com/github/stephengold/vsport/BaseApplication.java
+++ lib/src/main/java/com/github/stephengold/vsport/BaseApplication.java
@@ -2827,8 +2827,18 @@ public abstract class BaseApplication {
             pBarrier.oldLayout(oldLayout);
             pBarrier.srcQueueFamilyIndex(VK10.VK_QUEUE_FAMILY_IGNORED);
 
+            int aspectMask;
+            if (newLayout == VK10.VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+                aspectMask = VK10.VK_IMAGE_ASPECT_DEPTH_BIT;
+                if (Utils.hasStencilComponent(format)) {
+                    aspectMask |= VK10.VK_IMAGE_ASPECT_STENCIL_BIT;
+                }
+            } else {
+                aspectMask = VK10.VK_IMAGE_ASPECT_COLOR_BIT;
+            }
+
             VkImageSubresourceRange range = pBarrier.subresourceRange();
-            range.aspectMask(VK10.VK_IMAGE_ASPECT_COLOR_BIT);
+            range.aspectMask(aspectMask);
             range.baseArrayLayer(0);
             range.baseMipLevel(0);
             range.layerCount(1);
@@ -2852,6 +2862,17 @@ public abstract class BaseApplication {
 
                 sourceStage = VK10.VK_PIPELINE_STAGE_TRANSFER_BIT;
                 destinationStage = VK10.VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+
+            } else if (oldLayout == VK10.VK_IMAGE_LAYOUT_UNDEFINED
+                    && newLayout == VK10.VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+                pBarrier.dstAccessMask(
+                        VK10.VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT
+                        | VK10.VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT);
+                pBarrier.srcAccessMask(0x0);
+
+                sourceStage = VK10.VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+                destinationStage
+                        = VK10.VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
 
             } else {
                 throw new IllegalArgumentException(
