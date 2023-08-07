95e791c89b48acb0ec152e4ae0739864d378026d
Stephen Gold <sgold@sonic.net>
2023-08-07 16:56:37 -0700

    HelloVSport:  add a 3rd geometry to the scene

--- apps/src/main/java/com/github/stephengold/vsport/test/HelloVSport.java
+++ apps/src/main/java/com/github/stephengold/vsport/test/HelloVSport.java
@@ -37,6 +37,8 @@ import com.github.stephengold.vsport.Mesh;
 import com.github.stephengold.vsport.TextureKey;
 import com.github.stephengold.vsport.Vertex;
 import com.github.stephengold.vsport.mesh.OctasphereMesh;
+import com.github.stephengold.vsport.mesh.RectangleMesh;
+import com.jme3.math.FastMath;
 import java.util.ArrayList;
 import java.util.List;
 import org.lwjgl.assimp.Assimp;
@@ -98,18 +100,30 @@ public class HelloVSport extends BaseApplication {
         AssimpUtils.extractTriangles(modelName, postFlags, indices, vertices);
         Mesh roomMesh = Mesh.newInstance(vertices);
 
+        Mesh squareMesh = new RectangleMesh();
+        squareMesh.dropNormals();
+
         // texture keys:
         TextureKey marsKey
                 = new TextureKey("classpath:/Textures/sss/2k_mars.jpg");
         TextureKey roomKey = new TextureKey(
                 "classpath:/Models/viking_room/viking_room.png",
                 true, FlipAxes.noFlip);
+        TextureKey photoKey
+                = new TextureKey("classpath:/Textures/texture.jpg");
 
+        // geometries:
         mars = new Geometry(globeMesh);
         mars.setProgram("Unshaded/Texture");
         mars.setTexture(marsKey);
         mars.scale(0.4f);
         mars.setTranslation(1f, 0f, 0.2f);
+
+        Geometry photo = new Geometry(squareMesh);
+        photo.setProgram("Unshaded/Texture");
+        photo.setTexture(photoKey);
+        photo.setOrientation(FastMath.PI, 0f, 0f, 1f);
+        photo.setTranslation(1f, 1.4f, -0.2f);
 
         Geometry room = new Geometry(roomMesh);
         room.setProgram("Unshaded/Texture");
