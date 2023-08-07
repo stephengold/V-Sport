34f6d6f8236ab0be56ed1b475ef783a6564e5579
Stephen Gold <sgold@sonic.net>
2023-08-07 15:19:23 -0700

    apps: add a rotating globe to HelloVSport

--- apps/src/main/java/com/github/stephengold/vsport/test/HelloVSport.java
+++ apps/src/main/java/com/github/stephengold/vsport/test/HelloVSport.java
@@ -36,6 +36,7 @@ import com.github.stephengold.vsport.Geometry;
 import com.github.stephengold.vsport.Mesh;
 import com.github.stephengold.vsport.TextureKey;
 import com.github.stephengold.vsport.Vertex;
+import com.github.stephengold.vsport.mesh.OctasphereMesh;
 import java.util.ArrayList;
 import java.util.List;
 import org.lwjgl.assimp.Assimp;
@@ -46,6 +47,17 @@ import org.lwjgl.assimp.Assimp;
  * @author Stephen Gold sgold@sonic.net
  */
 public class HelloVSport extends BaseApplication {
+    // *************************************************************************
+    // fields
+
+    /**
+     * spinning globe
+     */
+    private static Geometry mars;
+    /**
+     * time when the application started
+     */
+    private static long startTime;
     // *************************************************************************
     // new methods exposed
 
@@ -73,20 +85,35 @@ public class HelloVSport extends BaseApplication {
      */
     @Override
     public void initialize() {
+        startTime = System.nanoTime();
+
+        // meshes:
+        int numRefineSteps = 5;
+        Mesh globeMesh = OctasphereMesh.getMesh(numRefineSteps);
+
         String modelName = "/Models/viking_room/viking_room.obj";
         int postFlags = Assimp.aiProcess_DropNormals | Assimp.aiProcess_FlipUVs;
         List<Integer> indices = null;
         List<Vertex> vertices = new ArrayList<>();
         AssimpUtils.extractTriangles(modelName, postFlags, indices, vertices);
-        Mesh mesh = Mesh.newInstance(vertices);
+        Mesh roomMesh = Mesh.newInstance(vertices);
 
-        TextureKey textureKey = new TextureKey(
+        // texture keys:
+        TextureKey marsKey
+                = new TextureKey("classpath:/Textures/sss/2k_mars.jpg");
+        TextureKey roomKey = new TextureKey(
                 "classpath:/Models/viking_room/viking_room.png",
                 true, FlipAxes.noFlip);
 
-        Geometry geometry = new Geometry(mesh);
-        geometry.setProgram("Unshaded/Texture");
-        geometry.setTexture(textureKey);
+        mars = new Geometry(globeMesh);
+        mars.setProgram("Unshaded/Texture");
+        mars.setTexture(marsKey);
+        mars.scale(0.4f);
+        mars.setTranslation(1f, 0f, 0.2f);
+
+        Geometry room = new Geometry(roomMesh);
+        room.setProgram("Unshaded/Texture");
+        room.setTexture(roomKey);
     }
 
     /**
@@ -94,6 +121,12 @@ public class HelloVSport extends BaseApplication {
      */
     @Override
     public void render() {
+        if (mars != null) {
+            long nanoseconds = System.nanoTime() - startTime;
+            float rotationAngle = 2e-10f * nanoseconds; // in radians
+            mars.setOrientation(rotationAngle, 0f, 0f, 1f);
+        }
+
         super.render();
     }
 }
