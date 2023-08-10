5464c05a3e820dfb438d612c24860cf70d7568a9
Stephen Gold <sgold@sonic.net>
2023-08-10 12:15:43 -0700

    LogicalDevice:  check the return value of createCommandPool()

--- lib/src/main/java/com/github/stephengold/vsport/LogicalDevice.java
+++ lib/src/main/java/com/github/stephengold/vsport/LogicalDevice.java
@@ -814,6 +814,7 @@ public class LogicalDevice {
             Utils.checkForError(retCode, "create a command-buffer pool");
             long result = pHandle.get(0);
 
+            assert result != VK10.VK_NULL_HANDLE;
             return result;
         }
     }
