cad5ed2a6c3a8875d443a70a3a7d79b06d2786c8
Stephen Gold <sgold@sonic.net>
2023-08-15 14:39:35 -0700

    GlobalUniformValues: partial revert of previous commit

--- lib/src/main/java/com/github/stephengold/vsport/GlobalUniformValues.java
+++ lib/src/main/java/com/github/stephengold/vsport/GlobalUniformValues.java
@@ -166,7 +166,7 @@ public class GlobalUniformValues {
         // mat4 viewMatrix
         byteOffset = Utils.align(byteOffset, 16);
         Camera camera = BaseApplication.getCamera();
-        camera.updateMatrix(viewMatrix);
+        camera.updateViewMatrix(viewMatrix);
         viewMatrix.get(byteOffset, target);
         byteOffset += 4 * 4 * Float.BYTES;
 
