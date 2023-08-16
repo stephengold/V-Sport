3adb3cde73fe3fd0e128d9a2d4c1b9e8c0103eb4
Stephen Gold <sgold@sonic.net>
2023-08-16 16:15:52 -0700

    RectangleMesh:  use the TriangleFan topology

--- lib/src/main/java/com/github/stephengold/vsport/mesh/RectangleMesh.java
+++ lib/src/main/java/com/github/stephengold/vsport/mesh/RectangleMesh.java
@@ -33,8 +33,7 @@ import com.github.stephengold.vsport.Mesh;
 import com.github.stephengold.vsport.Topology;
 
 /**
- * An indexed TriangleList mesh that renders an axis-aligned rectangle in the
- * X-Y plane.
+ * A TriangleFan mesh that renders an axis-aligned rectangle in the X-Y plane.
  * <p>
  * In mesh space, the rectangle extends from (x0,y0,0) uv=(0,0) to (x2,y2,0)
  * uv=(1,1), with normals set to (0,0,zNorm).
@@ -65,35 +64,40 @@ public class RectangleMesh extends Mesh {
      */
     public RectangleMesh(
             float x0, float x2, float y0, float y2, float normalZ) {
-        super(Topology.TriangleList, 4);
-        // TODO use TriangleFan
+        super(Topology.TriangleFan, 4);
+        /*
+         * The correct winding order depends on the coordinates chosen
+         * and the direction of the normals.
+         */
+        if ((x2 - x0) * (y2 - y0) * normalZ < 0f) {
+            super.setPositions(
+                    x0, y0, 0f,
+                    x0, y2, 0f,
+                    x2, y2, 0f,
+                    x2, y0, 0f);
+            super.setUvs(
+                    0f, 0f,
+                    0f, 1f,
+                    1f, 1f,
+                    1f, 0f);
+        } else {
+            super.setPositions(
+                    x0, y0, 0f,
+                    x2, y0, 0f,
+                    x2, y2, 0f,
+                    x0, y2, 0f);
+            super.setUvs(
+                    0f, 0f,
+                    1f, 0f,
+                    1f, 1f,
+                    0f, 1f);
+        }
 
-        super.setPositions(
-                x0, y0, 0f,
-                x2, y0, 0f,
-                x2, y2, 0f,
-                x0, y2, 0f
-        );
         super.setNormals(
                 0f, 0f, normalZ,
                 0f, 0f, normalZ,
                 0f, 0f, normalZ,
                 0f, 0f, normalZ
         );
-        super.setUvs(
-                0f, 0f,
-                1f, 0f,
-                1f, 1f,
-                0f, 1f
-        );
-        /*
-         * The vertex order for right-handed winding depends on the vertex
-         * coordinates and also on the direction of the normal vector.
-         */
-        if ((x2 - x0) * (y2 - y0) * normalZ < 0f) {
-            super.setIndices(0, 2, 1, 0, 3, 2);
-        } else {
-            super.setIndices(0, 1, 2, 0, 2, 3);
-        }
     }
 }
