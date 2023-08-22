d35155eef734bc2caba271c515b24439a216ed79
Stephen Gold <sgold@sonic.net>
2023-08-22 11:18:45 -0700

    SpriteTest:  re-assign copyright

--- apps/src/main/java/com/github/stephengold/vsport/test/SpriteTest.java
+++ apps/src/main/java/com/github/stephengold/vsport/test/SpriteTest.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2022-2023, Stephen Gold and Yanis Boudiaf
+ Copyright (c) 2022-2023, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
