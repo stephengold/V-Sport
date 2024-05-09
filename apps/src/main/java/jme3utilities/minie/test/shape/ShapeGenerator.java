1988b47b00ed169d51eb9cc303a52e7f39a94011
stephengold <sgold@sonic.net>
2024-05-09 16:30:04 -0700

    ShapeGenerator: rename the "frustum" shape

--- apps/src/main/java/jme3utilities/minie/test/shape/ShapeGenerator.java
+++ apps/src/main/java/jme3utilities/minie/test/shape/ShapeGenerator.java
@@ -629,6 +629,10 @@ public class ShapeGenerator extends Generator {
                 result = nextCustomDome();
                 break;
 
+            case "customFrustum":
+                result = nextCustomFrustum();
+                break;
+
             case "customHemisphere":
                 result = nextCustomHemisphere();
                 break;
@@ -667,10 +671,6 @@ public class ShapeGenerator extends Generator {
 
             case "frame":
                 result = nextFrame();
-                break;
-
-            case "frustum":
-                result = nextCustomFrustum();
                 break;
 
             case "halfCylinder":
