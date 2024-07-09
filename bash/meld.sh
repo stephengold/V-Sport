72d363abfae4f12cc3dc5e4c8d84e03e599e9dc6
stephengold <sgold@sonic.net>
2024-07-09 13:58:27 -0700

    meld.sh:  configure for Linux by default

--- bash/meld.sh
+++ bash/meld.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-#GitDir=~/NetBeansProjects
-GitDir="/c/Users/sgold/My Documents/NetBeansProjects"
+GitDir=~/NetBeansProjects
+#GitDir="/c/Users/sgold/My Documents/NetBeansProjects"
 
 S2="$GitDir/sport"
 D2="$GitDir/V-Sport"
@@ -17,7 +17,7 @@ D5="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
 S6="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
 D6="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/tutorial"
 
-#Meld="/usr/bin/meld"
-Meld="/c/Program Files/Meld/meld"
+Meld="/usr/bin/meld"
+#Meld="/c/Program Files/Meld/meld"
 
 "$Meld" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S5" "$D5" --diff "$S6" "$D6"
