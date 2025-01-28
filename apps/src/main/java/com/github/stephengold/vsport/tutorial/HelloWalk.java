68b30712ab71a68fa76f7c02b5914229f628fd06
stephengold <sgold@sonic.net>
2025-01-27 22:02:12 -0800

    HelloWalk:  trivial refactoring

--- apps/src/main/java/com/github/stephengold/vsport/tutorial/HelloWalk.java
+++ apps/src/main/java/com/github/stephengold/vsport/tutorial/HelloWalk.java
@@ -108,12 +108,7 @@ public class HelloWalk
      */
     @Override
     public PhysicsSpace createSpace() {
-        PhysicsSpace result
-                = new PhysicsSpace(PhysicsSpace.BroadphaseType.DBVT);
-
-        // To enable the callbacks, register the application as a tick listener.
-        result.addTickListener(this);
-
+        PhysicsSpace result = configurePhysics();
         return result;
     }
 
@@ -272,5 +267,20 @@ public class HelloWalk
 
         // Set the background color to light blue.
         setBackgroundColor(Constants.SKY_BLUE);
+    }
+
+    /**
+     * Configure physics during startup.
+     *
+     * @return a new instance (not null)
+     */
+    private PhysicsSpace configurePhysics() {
+        PhysicsSpace result
+                = new PhysicsSpace(PhysicsSpace.BroadphaseType.DBVT);
+
+        // To enable the callbacks, register the application as a tick listener.
+        result.addTickListener(this);
+
+        return result;
     }
 }
