35b7ba8f1835f501b92634cabdf7ed9424625baa
stephengold <sgold@sonic.net>
2025-07-13 19:20:26 -0700

    whitespace

--- apps/src/main/java/com/github/stephengold/vsport/tutorial/HelloVehicle.java
+++ apps/src/main/java/com/github/stephengold/vsport/tutorial/HelloVehicle.java
@@ -125,8 +125,8 @@ public class HelloVehicle extends BasePhysicsApp<PhysicsSpace> {
         cornerLocations.add(new Vector3f(-halfWidth, undercarriageY, tailZ));
         cornerLocations.add(new Vector3f(+halfWidth, spoilerY, tailZ));
         cornerLocations.add(new Vector3f(-halfWidth, spoilerY, tailZ));
-        HullCollisionShape wedgeShape
-                = new HullCollisionShape(cornerLocations);
+        HullCollisionShape wedgeShape = new HullCollisionShape(cornerLocations);
+
         float mass = 5f;
         PhysicsVehicle vehicle = new PhysicsVehicle(wedgeShape, mass);
         vehicle.setSuspensionCompression(6f); // default=0.83
