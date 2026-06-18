fe7f2267498d4119e4e6d18425f669a8a7d53627
stephengold <sgold@sonic.net>
2026-06-18 00:23:44 -0700

    add support for Windows_ARM64, update copyright years

--- lib/src/main/java/com/github/stephengold/vsport/physics/BasePhysicsApp.java
+++ lib/src/main/java/com/github/stephengold/vsport/physics/BasePhysicsApp.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2022-2025 Stephen Gold
+ Copyright (c) 2022-2026 Stephen Gold
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -319,6 +319,8 @@ abstract public class BasePhysicsApp<T extends PhysicsSpace>
             PlatformPredicate.MACOS_ARM_64),
             new NativeDynamicLibrary("native/osx/x86_64",
             PlatformPredicate.MACOS_X86_64),
+            new NativeDynamicLibrary("native/windows/arm64",
+            PlatformPredicate.WIN_ARM_64),
             new NativeDynamicLibrary("native/windows/x86_64",
             PlatformPredicate.WIN_X86_64)
         };
