a2cf86273c053a1fff439a3ad90944cdf4a9e7dd
stephengold <sgold@sonic.net>
2026-06-19 13:47:48 -0700

    meld.sh:  refactoring

--- bash/meld.sh
+++ bash/meld.sh
@@ -5,19 +5,19 @@ set -e
 GitDir=~/NetBeansProjects
 #GitDir="/c/Users/sgold/My Documents/NetBeansProjects"
 
-S2="$GitDir/sport"
-D2="$GitDir/V-Sport"
+S1="$GitDir/sport"
+D1="$GitDir/V-Sport"
 
-S3="$GitDir/LbjExamples/"
-D3="$GitDir/V-Sport/"
+S2="$GitDir/LbjExamples/"
+D2="$GitDir/V-Sport/"
 
-S5="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples"
-D5="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
+S3="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples"
+D3="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
 
-S6="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
-D6="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/tutorial"
+S4="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
+D4="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/tutorial"
 
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
-"$Meld" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S5" "$D5" --diff "$S6" "$D6"
+"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4"
